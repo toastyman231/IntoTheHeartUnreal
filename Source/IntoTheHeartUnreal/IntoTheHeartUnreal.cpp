6609cb9d7791fca8ed9518275deb9890dc11bc53
toastyman231 <jmorgenstern15@gmail.com>
2023-03-20 01:33:04 -0400

    Project initialization

---
+++ Source/IntoTheHeartUnreal/IntoTheHeartUnreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IntoTheHeartUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IntoTheHeartUnreal, "IntoTheHeartUnreal" );
---
+++ Source/IntoTheHeartUnreal/IntoTheHeartUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IntoTheHeartUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTOTHEHEARTUNREAL_API AIntoTheHeartUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
