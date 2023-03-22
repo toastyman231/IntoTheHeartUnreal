81e5d38db8d8c0d32696cb8fdd6b5ed70f8d4c76
toastyman231 <jmorgenstern15@gmail.com>
2023-03-21 20:09:33 -0400

    Added basic player controller

---
+++ Source/IntoTheHeartUnreal/OverworldCharacter.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OverworldCharacter.h"
+
+// Sets default values
+AOverworldCharacter::AOverworldCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AOverworldCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOverworldCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AOverworldCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveFB"), this, &AOverworldCharacter::MoveFB);
+	PlayerInputComponent->BindAxis(TEXT("MoveLR"), this, &AOverworldCharacter::MoveLR);
+	PlayerInputComponent->BindAxis(TEXT("RotateYaw"), this, &AOverworldCharacter::RotateYaw);
+	PlayerInputComponent->BindAxis(TEXT("RotatePitch"), this, &AOverworldCharacter::RotatePitch);
+}
+
+void AOverworldCharacter::MoveFB(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value * MoveSpeed);
+}
+
+void AOverworldCharacter::MoveLR(float Value)
+{
+	AddMovementInput(-GetActorRightVector(), Value * MoveSpeed);
+}
+
+void AOverworldCharacter::RotateYaw(float Value)
+{
+	AddControllerYawInput(Value * RotationSpeed);
+}
+
+void AOverworldCharacter::RotatePitch(float Value)
+{
+	AddControllerPitchInput(Value * RotationSpeed);
+}
+
---
+++ Source/IntoTheHeartUnreal/OverworldCharacter.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Camera/CameraComponent.h"
+#include "OverworldCharacter.generated.h"
+
+UCLASS()
+class INTOTHEHEARTUNREAL_API AOverworldCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AOverworldCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveFB(float Value);
+	void MoveLR(float Value);
+	void RotateYaw(float Value);
+	void RotatePitch(float Value);
+
+private:
+	UPROPERTY(EditAnywhere)
+		float MoveSpeed = 1.0f;
+
+	UPROPERTY(EditAnywhere)
+		float RotationSpeed = 1.0f;
+
+	UPROPERTY(EditDefaultsOnly)
+		UCameraComponent* PlayerCamera;
+
+};
