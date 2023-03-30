f2bbbdbd0dd393d8f9a1bd25c456d33bb921c0f3
Grant Waldow <38268357+gwaldow@users.noreply.github.com>
2023-03-30 19:14:20 -0400

    Puzzle minigame implemented

---
+++ Source/IntoTheHeartUnreal/PuzzlePawn.cpp
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/InputComponent.h"
+
+APuzzlePawn::APuzzlePawn()
+{
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create the SpringArm component
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->TargetArmLength = 500.f;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->CameraLagSpeed = 3.f;
+	//SpringArm->bUsePawnControlRotation = true;
+
+	// Create the Camera component
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+
+}
+
+void APuzzlePawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APuzzlePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APuzzlePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Bind the mouse input axis for camera rotation
+	PlayerInputComponent->BindAxis("MouseX", this, &APuzzlePawn::OnMouseDragX);
+	PlayerInputComponent->BindAxis("MouseY", this, &APuzzlePawn::OnMouseDragY);
+}
+
+void APuzzlePawn::OnMouseDragX(float AxisValue)
+{
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (PlayerController->IsInputKeyDown(EKeys::LeftMouseButton))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("OnMouseDragX: %f"), AxisValue);
+			float DeltaTime = GetWorld()->GetDeltaSeconds();
+
+			// Apply the mouse movement to the SpringArm rotation
+			FRotator NewRotation = SpringArm->GetComponentRotation();
+			NewRotation.Yaw += AxisValue;
+
+			// Set the new rotation for the SpringArm
+			SpringArm->SetWorldRotation(NewRotation);
+		}
+	}
+}
+
+void APuzzlePawn::OnMouseDragY(float AxisValue)
+{
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (PlayerController->IsInputKeyDown(EKeys::LeftMouseButton))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("OnMouseDragY: %f"), AxisValue);
+			float DeltaTime = GetWorld()->GetDeltaSeconds();
+			// Apply the mouse movement to the SpringArm rotation
+			FRotator NewRotation = SpringArm->GetComponentRotation();
+			NewRotation.Pitch = FMath::Clamp(NewRotation.Pitch - AxisValue, -80.0f, 80.0f);
+
+			// Set the new rotation for the SpringArm
+			SpringArm->SetWorldRotation(NewRotation);
+			UE_LOG(LogTemp, Warning, TEXT("SpringArm Rotation: %s"), *NewRotation.ToString());
+		}
+	}
+}
+
+
---
+++ Source/IntoTheHeartUnreal/PuzzlePawn.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PuzzlePawn.generated.h"
+
+UCLASS()
+class INTOTHEHEARTUNREAL_API APuzzlePawn : public APawn
+{
+    GENERATED_BODY()
+
+public:
+    APuzzlePawn();
+
+    virtual void Tick(float DeltaTime) override;
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+        class UCameraComponent* Camera;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+        class USpringArmComponent* SpringArm;
+
+    void OnMouseDragX(float AxisValue);
+    void OnMouseDragY(float AxisValue);
+
+    FVector2D LastMousePosition;
+};
